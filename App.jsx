86a6c32871736c8260194f7a9a534fce119c2869
PeterLineup <lineuppeter@gmail.com>
2023-05-13 22:32:37 +0530

    this commit is for adding navigators

--- App.jsx
+++ App.jsx
@@ -1,15 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import 'react-native-gesture-handler';
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import LoginScreen from './src/screens/login/Login';
+import HomeScreen from './src/screens/home/Home';
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  return (
-    <View>
-      <Text>App test</Text>
+  return (  
+    // <Stack.Navigator>
+    //   <Stack.Screen name="login_route" component={LoginScreen} />
+    //   <Stack.Screen name="home_route" component={HomeScreen} />
+    // </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="login_route" title="testing" component={LoginScreen} />
+        <Stack.Screen name="home_route" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-    </View>
-  )
-}
+export default App;
 
-export default App
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
