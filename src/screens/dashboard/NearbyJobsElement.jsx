dc4db50e3ae259abd6af7d3f87dbc37927cea41b
sidheshLineup <sidheshlineup@gmail.com>
2023-12-09 21:17:34 +0530

    resolved the nearby job issue for title and description

--- src/screens/dashboard/NearbyJobsElement.jsx
+++ src/screens/dashboard/NearbyJobsElement.jsx
@@ -15,6 +15,7 @@ import Carousel from 'react-native-snap-carousel';
 import useLoginStore from '../../store/authentication/login.store';
 import Icon, {Icons} from '../../components/Icons';
 import SeeAll from '../see-all/SeeAll';
+import {primaryBGColor} from '../../helper/utils/colors';
 
 const nearbyJobsColorSchemes = ['#87C4FF', '#739072', '#CE5A67', '#ECEE81'];
 const NearbyJobsElement = ({
@@ -25,7 +26,7 @@ const NearbyJobsElement = ({
   location,
 }) => {
   const {loggedInUser} = useLoginStore();
-  
+
   if (!location) {
     return (
       <CommonMessageForNearByJobs
@@ -99,9 +100,9 @@ const handleBookmarkPress = () => {
   // Handle bookmark button press logic here
   console.log('Bookmark button pressed!');
 };
-const renderItemsNearbyJobs = ({item, index, navigation,nearbyjobs}) => {
+const renderItemsNearbyJobs = ({item, index, navigation, nearbyjobs}) => {
   const isLastSlide = Object.keys(item).length;
-  console.log("isLastSlide",isLastSlide,item);
+  console.log('isLastSlide', isLastSlide, item);
   if (!isLastSlide) {
     return (
       <TouchableOpacity
@@ -116,84 +117,81 @@ const renderItemsNearbyJobs = ({item, index, navigation,nearbyjobs}) => {
           type={Icons.Entypo}
           name={'chevron-with-circle-right'}
           size={45}
-          color={'green'}
+          color={primaryBGColor}
         />
       </TouchableOpacity>
     );
   }
   if (isLastSlide) {
-  return (
-    <ImageBackground
-      source={require('../../assets/images/nearby-jobs-skin-1.png')}
-      style={[tw`w-full h-48 rounded-3 bg-[${item?.styles?.bgcolor}]`]}
-      resizeMode="cover">
-      <Pressable
-        onPress={() => {
-          console.log('pressed');
-          navigation.navigate('ApplyNow', {jobDetails: item, id: item._id});
-        }}
-        key={item._id}
-        style={tw`w-full h-48 rounded-3`}>
-        <View style={tw`flex flex-row justify-between p-4 h-35`}>
-          {/* <View> 
-          <Image source={item.image} style={tw`w-15 h-15 rounded-full`} /> 
-         </View> */}
-          <View style={tw` items-center p-2`}>
+    return (
+      <ImageBackground
+        source={require('../../assets/images/nearby-jobs-skin-1.png')}
+        style={[tw`w-full h-48 rounded-3 bg-[${item?.styles?.bgcolor}]`]}
+        resizeMode="cover">
+        <Pressable
+          onPress={() => {
+            console.log('pressed');
+            navigation.navigate('ApplyNow', {jobDetails: item, id: item._id});
+          }}
+          key={item._id}
+          style={tw`w-full h-48 rounded-3`}>
+          <View style={tw`flex flex-row justify-between p-4 h-35`}>
+            <View style={tw`p-2 w-[70%]`}>
+              <Text
+                style={[
+                  tw`text-[${item?.styles?.color}] text-[20px]`,
+                  {fontFamily: 'Poppins-SemiBold'},
+                ]}
+                numberOfLines={1}
+                ellipsizeMode="tail">
+                {item.position}
+              </Text>
+              <Text
+                style={[
+                  tw`text-[${item?.styles?.color}] text-[16px]`,
+                  {fontFamily: 'Poppins-Regular'},
+                ]}
+                numberOfLines={1}
+                ellipsizeMode="tail">
+                {item.description}
+              </Text>
+            </View>
+            <View style={tw`items-end`}>
+              <Pressable
+                onPress={handleBookmarkPress}
+                style={({pressed}) => [
+                  tw`p-2 rounded-full ${pressed ? 'bg-black/20' : ''}`,
+                ]}>
+                <Icon
+                  type={Icons.Ionicons}
+                  name="bookmark"
+                  size={24}
+                  color={'white'}
+                />
+              </Pressable>
+            </View>
+          </View>
+
+          <View style={tw`flex flex-row justify-between px-5 py-2`}>
             <Text
               style={[
-                tw`text-[${item?.styles?.color}] text-[20px] `,
-                {fontFamily: 'Poppins-SemiBold'},
-              ]}
-              numberOfLines={1}
-              ellipsizeMode="tail">
-              {item.position}
+                tw`text-[${item?.styles?.color}] text-[16px]`,
+                {fontFamily: 'Poppins-Regular'},
+              ]}>
+              ₹. {item.salary}
             </Text>
             <Text
               style={[
                 tw`text-[${item?.styles?.color}] text-[16px]`,
                 {fontFamily: 'Poppins-Regular'},
-              ]}
-              numberOfLines={1}
-              ellipsizeMode="tail">
-              {item.description}
+              ]}>
+              {item.location?.name}
             </Text>
           </View>
-          <View style={tw`items-end`}>
-            <Pressable
-              onPress={handleBookmarkPress}
-              style={({pressed}) => [
-                tw`p-2 rounded-full ${pressed ? 'bg-black/20' : ''}`,
-              ]}>
-              <Icon
-                type={Icons.Ionicons}
-                name="bookmark"
-                size={24}
-                color={'white'}
-              />
-            </Pressable>
-          </View>
-        </View>
-
-        <View style={tw`flex flex-row justify-between px-5 py-2`}>
-          <Text
-            style={[
-              tw`text-[${item?.styles?.color}] text-[16px]`,
-              {fontFamily: 'Poppins-Regular'},
-            ]}>
-            ₹. {item.salary}
-          </Text>
-          <Text
-            style={[
-              tw`text-[${item?.styles?.color}] text-[16px]`,
-              {fontFamily: 'Poppins-Regular'},
-            ]}>
-            {item.location?.name}
-          </Text>
-        </View>
-      </Pressable>
-    </ImageBackground>
-  );
-            }
+        </Pressable>
+      </ImageBackground>
+    );
+  }
 };
 
 function CommonMessageForNearByJobs({title, language}) {
