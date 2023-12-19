0fb6495a2f1a1d01a0ba4bd122153eb06bac1d17
sidheshLineup <sidheshlineup@gmail.com>
2023-12-20 00:23:48 +0530

    added env

--- env.js
+++ env.js
@@ -3,7 +3,8 @@ export const REACT_APP_ENV = 'local';
 // export const REACT_APP_ENV = 'test'
 // export const REACT_APP_ENV = 'production'
 
-export const REACT_APP_LOCAL_MODE = 'https://08b9-206-84-226-174.ngrok-free.app';
+export const REACT_APP_LOCAL_MODE =
+  'https://771a-103-196-79-115.ngrok-free.app';
 // export const REACT_APP_DEV_MODE = 'http:// 192.168.0.124';
 // export const REACT_APP_TEST_MODE = 'http://192.168.177.34:3030';
 // export const REACT_APP_PRODUCTION_MODE = 'http://192.168.177.34:3030';
