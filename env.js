c074848a2a0a6614b6431f98a218d9cf45cc403e
kammrepo <kaamappofficial@gmail.com>
2024-05-07 01:18:17 +0530

    inital commit

--- env.js
+++ env.js
@@ -1,11 +1,11 @@
-export const REACT_APP_ENV = 'local';
-// export const REACT_APP_ENV = 'dev'
+// export const REACT_APP_ENV = 'local';
+export const REACT_APP_ENV = 'dev'
 // export const REACT_APP_ENV = 'test'
 // export const REACT_APP_ENV = 'production'
 
-export const REACT_APP_LOCAL_MODE =
-  'https://42cc-2402-3a80-1b3f-4aca-dcfc-f968-3024-4113.ngrok-free.app';
+// export const REACT_APP_LOCAL_MODE =
+//   'https://42cc-2402-3a80-1b3f-4aca-dcfc-f968-3024-4113.ngrok-free.app';
 
-// export const REACT_APP_DEV_MODE = 'http:// 192.168.0.124';
+export const REACT_APP_DEV_MODE = 'http://localhost:3030';
 // export const REACT_APP_TEST_MODE = 'http://192.168.177.34:3030';
 // export const REACT_APP_PRODUCTION_MODE = 'http://192.168.177.34:3030';
