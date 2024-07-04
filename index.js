e506a868365327dcb14704b588cf218707f8c73a
sidheshLineup <sidheshlineup@gmail.com>
2024-07-04 22:35:09 +0530

    added push notification action buttons open and reply and code to navigate to respective screen, and send the chat message from push notification itself using reply button

--- index.js
+++ index.js
@@ -8,21 +8,37 @@ import {name as appName} from './app.json';
 
 import notifee, {AndroidImportance, EventType} from '@notifee/react-native';
 import messaging from '@react-native-firebase/messaging';
+import {navigate} from './navigation';
+import useChatStore from './src/store/chat.store';
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {
-  console.log('Message handled in the background!', remoteMessage);
+  // console.log('Message handled in the background!', remoteMessage);
   await onMessageReceived(remoteMessage);
 });
 notifee.onBackgroundEvent(async ({type, detail}) => {
   const {notification, pressAction} = detail;
-  console.log(
-    '🚀 ~ notifee.onBackgroundEvent ~ detail:',
-    JSON.stringify(detail, null, 4),
-  );
-  // Remove the notification
-  // Or perform another action
-  if (type === EventType.ACTION_PRESS) {
+
+  const {data} = detail.notification.data;
+  if (
+    type === EventType.ACTION_PRESS &&
+    detail.pressAction.id === 'open-chat'
+  ) {
+    navigate('Chat', {
+      appliedJobId: data['appliedJobId'],
+      chatid: data['chatid'],
+      name: data['name'],
+    });
     await notifee.cancelNotification(notification.id);
+  }
+  if (type === EventType.ACTION_PRESS && detail.pressAction.id === 'reply') {
+    const chat_message = {
+      messageType: 'text',
+      text: detail.input,
+      createdat: new Date().toISOString(),
+      isseen: false,
+    };
+    useChatStore.getState().sendChatMessage(data['chatid'], chat_message);
+    // await notifee.cancelNotification(detail.notification.id);
   }
   //   await onMessageReceived(remoteMessage);
 });
@@ -30,6 +46,7 @@ notifee.onBackgroundEvent(async ({type, detail}) => {
 async function onMessageReceived(message) {
   try {
     const notifyObj = JSON.parse(message.data.notifee);
+    // const data = notifyObj.data;
     const channelId = await notifee.createChannel({
       id: 'default',
       name: 'Important Notifications',
