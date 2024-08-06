a1ad2f399202b21e4f58bd5c1718bd524799d124
kammrepo <kaamappofficial@gmail.com>
2024-08-06 13:40:42 +0530

    added approval screen

--- src/screens/incremental/RequestScreen.jsx
+++ src/screens/incremental/RequestScreen.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   View,
   Text,
@@ -8,23 +8,24 @@ import {
   ActivityIndicator,
   useColorScheme,
 } from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
+import { Button } from 'react-native-paper';
 import useLoaderStore from '../../store/loader.store';
 import dayjs from 'dayjs';
 import tw from 'twrnc';
-import {useNavigation} from '@react-navigation/native';
-import {useApprovalStore} from '../../store/approval.store'
+import useApprovalStore from '../../store/approval.store';
 
 const RequestScreen = () => {
   useColorScheme();
-  const {clearApproval, getRequests,approval} =
-  useApprovalStore();
-  const {isLoading} = useLoaderStore();
+  const { clearApproval, getRequests, approval } = useApprovalStore();
+  const { isLoading } = useLoaderStore();
   const [refreshing, setRefreshing] = useState(false);
 
   useEffect(() => {
     getRequests();
     return () => {
-        clearApproval();
+      clearApproval();
     };
   }, []);
 
@@ -34,21 +35,25 @@ const RequestScreen = () => {
     setRefreshing(false);
   };
 
-  return isLoading && !approval?.length ? (
-    <MenuLoadingComponent />
-  ) : approval?.length == 0 ? (
-    <NoDataMenuComponent message={'No Requests found!'} />
-  ) : (
-    <>
-      <Text style={[tw`my-1 text-right text-black dark:text-white`]}>
-        Total: {approval?.length}
-      </Text>
-      <JobApplicationsComponent
-        refreshing={refreshing}
-        refreshJobData={refreshJobData}
-        isLoading={isLoading}
-      />
-    </>
+  return (
+    <SafeAreaView style={tw`flex-1 bg-white dark:bg-black mx-2`}>
+      {isLoading && !approval?.length ? (
+        <MenuLoadingComponent />
+      ) : approval?.length === 0 ? (
+        <NoDataMenuComponent message={'No Requests found!'} />
+      ) : (
+        <>
+          <Text style={[tw`my-1 text-right text-black dark:text-white`]}>
+            Total: {approval?.length}
+          </Text>
+          <RequestApplicationsComponent
+            refreshing={refreshing}
+            refreshJobData={refreshJobData}
+            isLoading={isLoading}
+          />
+        </>
+      )}
+    </SafeAreaView>
   );
 };
 
@@ -57,7 +62,7 @@ export default RequestScreen;
 const MenuLoadingComponent = () => {
   return (
     <View style={tw`w-full bg-white dark:bg-gray-950 mt-5 gap-1 rounded`}>
-      {Array.from({length: 10}, (_, i) => i)?.map(data => (
+      {Array.from({ length: 10 }, (_, i) => i).map((data) => (
         <View
           key={data}
           style={tw`w-full h-15 bg-slate-200/50 dark:bg-gray-800 my-0.6 rounded-3`}
@@ -66,29 +71,33 @@ const MenuLoadingComponent = () => {
     </View>
   );
 };
-const NoDataMenuComponent = ({message}) => {
+
+const NoDataMenuComponent = ({ message }) => {
+  const navigation = useNavigation();
   return (
     <View style={tw`w-full h-full gap-4`}>
       <View style={tw`flex-1 justify-center items-center`}>
         <Text
           style={[
             tw`text-lg text-zinc-700 dark:text-zinc-400`,
-            {fontFamily: 'Poppins-SemiBold'},
-          ]}>
+            { fontFamily: 'Poppins-SemiBold' },
+          ]}
+        >
           {message}
         </Text>
+        <Button onPress={() => navigation.navigate('Dashboard')}>Home</Button>
       </View>
     </View>
   );
 };
 
-const JobApplicationsComponent = ({refreshing, refreshJobData, isLoading}) => {
-    const {clearApproval, getRequests,approval} =
-    useApprovalStore();
-  const fetchMoreJobApplications = async () => {
+const RequestApplicationsComponent = ({ refreshing, refreshJobData, isLoading }) => {
+  const { clearApproval, getRequests, approval } = useApprovalStore();
+
+  const fetchMoreRequestApplications = async () => {
     if (approval.length) {
-      let {skip, total, limit} = aprovalList;
-      if (aprovalList && skip < total) {
+      let { skip, total, limit } = approval;
+      if (approval && skip < total) {
         skip = skip + limit;
         await getRequests(skip, limit);
       }
@@ -98,9 +107,9 @@ const JobApplicationsComponent = ({refreshing, refreshJobData, isLoading}) => {
   return (
     <FlatList
       style={tw`mb-16`}
-      data={aprovalList?.data}
-      renderItem={({item}) => <JobCard job={item} />}
-      keyExtractor={item => item._id}
+      data={approval || []}
+      renderItem={({ item }) => <ApplicationCard data={item} />}
+      keyExtractor={(item) => item._id}
       refreshControl={
         <RefreshControl refreshing={refreshing} onRefresh={refreshJobData} />
       }
@@ -108,73 +117,68 @@ const JobApplicationsComponent = ({refreshing, refreshJobData, isLoading}) => {
         <FooterComponent isLoading={isLoading} refreshing={refreshing} />
       }
       onEndReachedThreshold={0.2}
-      onEndReached={fetchMoreJobApplications}
+      onEndReached={fetchMoreRequestApplications}
     />
   );
 };
 
-export const JobCard = ({job}) => {
-    console.log("Job ",job);
+const ApplicationCard = ({ data }) => {
   const navigation = useNavigation();
 
   return (
     <TouchableOpacity
-      key={job?._id}
-      onPress={() => {
-        navigation.navigate('Chat', {
-          chatid: job.chatid,
-          appliedJobId: job._id,
-          name: `${job?.employerDetails?.firstname} ${job?.employerDetails?.lastname}`,
-        });
-      }}
-      style={tw`w-full bg-white dark:bg-gray-800 my-0.6 px-5 gap-1 py-3 rounded relative border-l-4 border-r-4 shadow border-emerald-500`}>
+      key={data?._id}
+      style={tw`w-full bg-white dark:bg-gray-800 my-0.6 px-5 gap-1 py-3 rounded relative border-l-4 border-r-4 shadow border-emerald-500`}
+    >
       <Text
         style={[
           tw`text-[16px] text-black dark:text-white`,
-          {fontFamily: 'Poppins-SemiBold'},
-        ]}>
-        {job?.jobDetails?.jobtitle}
+          { fontFamily: 'Poppins-SemiBold' },
+        ]}
+      >
+        {data?.jobDetails?.jobtitle}
       </Text>
-
+      <View style={tw`flex-row`}>
       <Text
         style={[
           tw`text-xs text-gray-700 dark:text-gray-300 absolute top-[12px] right-[20px]`,
-          {fontFamily: 'Poppins-Regular'},
-        ]}>
-        Status :{' '}
-        <Text style={[tw`text-xs`, {fontFamily: 'Poppins-SemiBold'}]}>
-          {job?.status}
+          { fontFamily: 'Poppins-Regular' },
+        ]}
+      >
+        Status:{' '}
+        <Text style={[tw`text-xs`, { fontFamily: 'Poppins-SemiBold' }]}>
+          {data?.status}
         </Text>
       </Text>
-
-      <View style={tw`flex-row justify-between`}>
         <Text
           style={[
             tw`text-xs text-gray-700 dark:text-gray-300`,
-            {fontFamily: 'Poppins-Regular'},
-          ]}>
-          Organization:{' '}
-          <Text style={[tw`text-xs`, {fontFamily: 'Poppins-SemiBold'}]}>
-            {job?.employerDetails?.firstname} {job?.employerDetails?.lastname}
-          </Text>
-        </Text>
-
-        <Text
-          style={[
-            tw`text-xs text-gray-700 dark:text-gray-300`,
-            {fontFamily: 'Poppins-Regular'},
-          ]}>
-          Applied On:{' '}
-          <Text style={[tw`text-xs`, {fontFamily: 'Poppins-SemiBold'}]}>
-            {dayjs(job?.jobDetails?.createdat).format('DD MMM YYYY')}
+            { fontFamily: 'Poppins-Regular' },
+          ]}
+        >
+          Request for:{' '}
+          <Text style={[tw`text-xs`, { fontFamily: 'Poppins-SemiBold' }]}>
+            {data?.requesttype}
           </Text>
         </Text>
       </View>
+
+      <Text
+          style={[
+            tw`text-xs text-gray-700 dark:text-gray-300`,
+            { fontFamily: 'Poppins-Regular' },
+          ]}
+        >
+          Applied On:{' '}
+          <Text style={[tw`text-xs`, { fontFamily: 'Poppins-SemiBold' }]}>
+            {dayjs(data?.createdat).format('MMMM D, YYYY h:mm A')}
+          </Text>
+        </Text>
     </TouchableOpacity>
   );
 };
 
-const FooterComponent = ({isLoading, refreshing}) => {
+const FooterComponent = ({ isLoading, refreshing }) => {
   return (
     <View style={tw``}>
       <ActivityIndicator
